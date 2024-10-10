4b6e81a7596645994356024521ad2d7102803a8d
Juan Cabello <jcabelloc@itana.pe>
2024-10-09 20:46:03 -0500

    Modularizar el App

--- app.js
+++ app.js
@@ -1,49 +1,10 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    if (url=== '/') {
-        res.write('<html>');
-        res.write('<head><title>Mi primera pagina</title></head>');
-        res.write('<body><form action="/mensaje" method="POST"><input type="text" id="miinput" name="nombre-producto"></input><button type="submit">Enviar</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-    if (url === '/mensaje' && method === 'POST') {
+const routes = require('./routes')
 
 
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-        return req.on('end', () => {
-            const txtBody = Buffer.concat(body).toString();
-            console.log(txtBody)
-            const mensaje = txtBody.split('=')[1];
-            //fs.writeFileSync('mensaje.txt', mensaje);
-            fs.writeFile('mensaje.txt', mensaje, err => {
-                res.statusCode = '302';
-                res.setHeader('Location', '/')
-                return res.end();
-            });
-        })
-        /*
-        res.statusCode = '302';
-        res.setHeader('Location', '/')
-        return res.end(); */
+console.log(routes.descripcion);
 
-
-    }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Mi primera pagina</title></head>');
-    res.write('<body><h1> Hola desde el Servidor Node.JS</h1></body>');
-    res.write('</html>');
-    res.end();
-
-})
+const server = http.createServer(routes.enrutador);
 
 server.listen(3000);
